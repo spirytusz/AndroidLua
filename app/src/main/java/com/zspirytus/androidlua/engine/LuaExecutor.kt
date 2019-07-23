1e201cc833e9b5eaef2127a2db8b8495b8fc7ebc
zhangkunwei <zhangkunwei@yy.com>
2019-07-23 12:41:22 +0800

    解压任务在子线程执行.

--- app/src/main/java/com/zspirytus/androidlua/engine/LuaExecutor.kt
+++ app/src/main/java/com/zspirytus/androidlua/engine/LuaExecutor.kt
@@ -1,24 +1,42 @@
 package com.zspirytus.androidlua.engine
 
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
 import com.zspirytus.androidlua.utils.ZipFileUtils
 import java.io.File
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 import java.util.zip.ZipFile
 
 class LuaExecutor {
 
+    var scriptDataFetcher: ScriptPkgDataFetcher? = null
+    private var mHandler: Handler? = null
+    private var mThreadPool: ExecutorService? = null
+
     init {
         System.loadLibrary("luabridge")
+        mThreadPool = Executors.newSingleThreadExecutor()
+        mHandler = Handler(Looper.getMainLooper())
     }
 
-    var scriptDataFetcher: ScriptPkgDataFetcher? = null
-
     fun runScriptPkg(scriptPkg: File, configFile: String) {
-        initScriptPkg(scriptPkg)
-        val zipFile = ZipFile(scriptPkg)
-        val config = ZipFileUtils.getFileContentFromZipFile(zipFile, configFile)
-        val luaScriptPaths = config.split("\r\n")
-        val luaScript = ZipFileUtils.getFilesContentFromZipFile(zipFile, luaScriptPaths)
-        startScript(luaScript)
+        Log.d("LuaExecutor", "accept at Thread: ${Thread.currentThread().name}")
+        mThreadPool?.execute {
+            Log.d("LuaExecutor", "execute unzip at Thread: ${Thread.currentThread().name}")
+            val start = System.currentTimeMillis()
+            initScriptPkg(scriptPkg)
+            val zipFile = ZipFile(scriptPkg)
+            val config = ZipFileUtils.getFileContentFromZipFile(zipFile, configFile)
+            val luaScriptPaths = config.split("\r\n")
+            val luaScript = ZipFileUtils.getFilesContentFromZipFile(zipFile, luaScriptPaths)
+            Log.d("USE_TIME", "${System.currentTimeMillis() - start} ms")
+            mHandler?.post {
+                Log.d("LuaExecutor", "callback at Thread: ${Thread.currentThread().name}")
+                startScript(luaScript)
+            }
+        }
     }
 
     fun runScript(script: String) {
