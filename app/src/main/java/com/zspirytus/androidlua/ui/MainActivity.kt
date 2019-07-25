1d29be545b1bf8cb6992591537fe27736bd132d4
zhangkunwei <zhangkunwei@yy.com>
2019-07-25 10:37:04 +0800

    优化代码

--- app/src/main/java/com/zspirytus/androidlua/ui/MainActivity.kt
+++ app/src/main/java/com/zspirytus/androidlua/ui/MainActivity.kt
@@ -6,8 +6,10 @@ import android.os.Environment
 import android.support.v4.app.ActivityCompat
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.widget.Toast
 import com.zspirytus.androidlua.engine.LuaExecutor
 import com.zspirytus.androidlua.R
+import com.zspirytus.zspermission.PermissionFragment
 import kotlinx.android.synthetic.main.activity_main.*
 import java.io.File
 
@@ -16,37 +18,37 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        requestPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        val permissions = arrayOf(
+            android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+        )
+        PermissionFragment.getInstance(this).requestPermission(permissions) { isGranted, deniedPermissions, _ ->
+            when {
+                isGranted -> onGranted()
+                else -> deniedPermissions.forEach {
+                    Log.e(TAG, "not grant: $it")
+                }
+            }
+        }
     }
 
-    private fun requestPermission(vararg permissions: String) {
-        ActivityCompat.requestPermissions(this, permissions, REQUEST_CODE)
-    }
-
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == REQUEST_CODE) {
-            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                start_btn.setOnClickListener {
-                    if (!LuaExecutor.getInstance().isScriptRunning()) {
-                        val scriptPkg = File(Environment.getExternalStorageDirectory(), "script.zip")
-                        val config = "config"
-                        Log.d(TAG, "startScript: ${LuaExecutor.getInstance().runScriptPkg(scriptPkg, config)}" +
-                            " isScriptRunning: ${LuaExecutor.getInstance().isScriptRunning()}")
-                    }
-                }
-                stop_btn.setOnClickListener {
-                    if (LuaExecutor.getInstance().isScriptRunning()) {
-                        Log.d(TAG, "stopScript: ${LuaExecutor.getInstance().stopScript()} " +
-                            "isScriptRunning: ${LuaExecutor.getInstance().isScriptRunning()}")
-                    }
-                }
+    private fun onGranted() {
+        start_btn.setOnClickListener {
+            if (!LuaExecutor.getInstance().isScriptRunning()) {
+                val scriptPkg = File(Environment.getExternalStorageDirectory(), "script.zip")
+                val config = "config"
+                Log.d(TAG, "startScript: ${LuaExecutor.getInstance().runScriptPkg(scriptPkg, config)}" +
+                    " isScriptRunning: ${LuaExecutor.getInstance().isScriptRunning()}")
+            }
+        }
+        stop_btn.setOnClickListener {
+            if (LuaExecutor.getInstance().isScriptRunning()) {
+                Log.d(TAG, "stopScript: ${LuaExecutor.getInstance().stopScript()} " +
+                    "isScriptRunning: ${LuaExecutor.getInstance().isScriptRunning()}")
             }
         }
     }
 
     companion object {
         private const val TAG = "MainActivity"
-        private const val REQUEST_CODE = 233
     }
 }
